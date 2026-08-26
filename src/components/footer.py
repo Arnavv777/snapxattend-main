b0748894005fd974e089f3234f6c5149c574436e
Arnav Singh <arnavsingh2006.21@gmail.com>
2026-08-26 19:34:12 +0530

    streamlit setup + homescreen

---
+++ src/components/footer.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def footer_home():
+
+    st.markdown(f"""
+
+        <div style ="margin-top:2rem;display:flex ; justify-content:center;items-align:center">
+        <p style = "color:white"> Created with ❤️ by <b>Arnav</b></p>
+        </div>
+
+
+            """,unsafe_allow_html=True) 
